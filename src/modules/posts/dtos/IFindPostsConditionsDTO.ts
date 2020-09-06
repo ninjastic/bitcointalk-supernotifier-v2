62697889b2b5a5aecf166bb7e4544705ce4c0823
Ninjastic <ninjasticdev@protonmail.com>
2020-09-05 22:02:46 -0300

    fix: find posts conditions conditional

--- src/modules/posts/dtos/IFindPostsConditionsDTO.ts
+++ src/modules/posts/dtos/IFindPostsConditionsDTO.ts
@@ -2,8 +2,8 @@ export default interface IFindPostsConditionsDTO {
   author?: string;
   content?: string;
   topic_id?: number;
-  after_date: string;
-  before_date: string;
+  after_date?: string;
+  before_date?: string;
   last?: number;
   after?: number;
 }
