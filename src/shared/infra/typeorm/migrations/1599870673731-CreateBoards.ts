ac2084fb13b053c3b4ca5a117794296f4b0c7d8c
Ninjastic <ninjasticdev@protonmail.com>
2020-09-12 01:38:13 -0300

    feat: create boards table

---
+++ src/shared/infra/typeorm/migrations/1599870673731-CreateBoards.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateBoards1599870673731 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'boards',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'board_id',
+            type: 'integer',
+            isUnique: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'parent_id',
+            type: 'integer',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('boards');
+  }
+}
