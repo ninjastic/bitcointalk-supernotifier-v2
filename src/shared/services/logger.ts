8b11faa8899bfef68cd916c357c79004da698658
ninjastic <ninjasticdev@protonmail.com>
2023-02-28 15:53:22 -0300

    feat: logger fatal errors

--- src/shared/services/logger.ts
+++ src/shared/services/logger.ts
@@ -5,4 +5,12 @@ const logger = pino({
   timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`
 });
 
+process.on('uncaughtException', err => {
+  logger.fatal(err);
+});
+
+process.on('unhandledRejection', err => {
+  logger.fatal(err);
+});
+
 export default logger;
