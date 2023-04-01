a06484875645beb7f4c60ffb9c937678afc19dcb
ninjastic <ninjasticdev@protonmail.com>
2023-03-31 23:56:26 -0300

    fix: global alert escaping html

--- src/shared/infra/telegram/commands/alertCommand.ts
+++ src/shared/infra/telegram/commands/alertCommand.ts
@@ -5,20 +5,16 @@ import IMenuContext from '../@types/IMenuContext';
 import SendGlobalNotificationService from '../services/SendGlobalNotificationService';
 
 const alertCommand = async (ctx: CommandContext<IMenuContext>): Promise<void> => {
-  if (String(ctx.from.id) === '608520255') {
-    const sendGlobalNotification = container.resolve(SendGlobalNotificationService);
-
-    const message = ctx.message.text.replace('/alert ', '');
-
-    await sendGlobalNotification
-      .execute(message)
-      .then(async () => {
-        await ctx.reply('The messages started to be sent.');
-      })
-      .catch(async () => {
-        await ctx.reply('Something went wrong.');
-      });
+  if (String(ctx.from.id) !== '608520255') {
+    return;
   }
+
+  const sendGlobalNotification = container.resolve(SendGlobalNotificationService);
+  const message = ctx.match;
+
+  await sendGlobalNotification.execute(message).catch(async () => {
+    await ctx.reply('Something went wrong.');
+  });
 };
 
 export default alertCommand;
--- src/shared/infra/telegram/services/SendGlobalNotificationService.ts
+++ src/shared/infra/telegram/services/SendGlobalNotificationService.ts
@@ -1,5 +1,4 @@
 import { injectable, inject, container } from 'tsyringe';
-import escape from 'escape-html';
 
 import logger from '../../../services/logger';
 import bot from '../index';
@@ -7,6 +6,12 @@ import bot from '../index';
 import IUsersRepository from '../../../../modules/users/repositories/IUsersRepository';
 
 import SetUserBlockedService from './SetUserBlockedService';
+import RedisProvider from '../../../container/providers/implementations/RedisProvider';
+
+type MessageSent = {
+  telegramId: string;
+  messageId: number;
+};
 
 @injectable()
 export default class SendGlobalNotificationService {
@@ -17,71 +22,81 @@ export default class SendGlobalNotificationService {
 
   public async execute(message: string): Promise<void> {
     const setUserBlocked = container.resolve(SetUserBlockedService);
+    const redisProvider = container.resolve(RedisProvider);
 
-    const users = await this.usersRepository.findAll(true);
-
+    const unblockedUsers = await this.usersRepository.findAll(true);
+    const messageIds: MessageSent[] = [];
     let successed = 0;
     let errored = 0;
 
-    await Promise.all(
-      users.map(async (user, index, array) => {
-        setTimeout(async () => {
-          await bot.instance.api
-            .sendMessage(user.telegram_id, escape(message), {
-              parse_mode: 'HTML',
-              reply_markup: {
-                remove_keyboard: true
-              }
-            })
-            .then(() => {
-              logger.info({ telegram_id: user.telegram_id, message }, 'Global notification was sent');
-              successed += 1;
-            })
-            .catch(async error => {
-              if (!error.response) {
-                logger.error(
-                  { error: error.message, telegram_id: user.telegram_id, message },
-                  'Error while sending Global Notification telegram message'
-                );
+    await bot.instance.api.sendMessage(608520255, `Starting to send the messages...`);
 
-                return;
-              }
-              if (
-                error.response.description === 'Forbidden: bot was blocked by the user' ||
-                error.response.description === 'Forbidden: user is deactivated'
-              ) {
-                logger.info(
-                  {
-                    error: error.response,
-                    telegram_id: user.telegram_id,
-                    message
-                  },
-                  'Telegram user marked as blocked'
-                );
-                await setUserBlocked.execute(user.telegram_id);
-              } else {
-                logger.error(
-                  {
-                    error: error.response,
-                    telegram_id: user.telegram_id,
-                    message
-                  },
-                  'Error while sending Global Notification telegram message'
-                );
-              }
+    const promises = unblockedUsers.map(
+      async (user, index) =>
+        new Promise<void>((resolve, reject) => {
+          setTimeout(async () => {
+            await bot.instance.api
+              .sendMessage(user.telegram_id, message, {
+                parse_mode: 'HTML',
+                disable_web_page_preview: true,
+                reply_markup: {
+                  remove_keyboard: true
+                }
+              })
+              .then(messageSent => {
+                logger.info({ telegram_id: user.telegram_id, message }, 'Global notification was sent');
+                messageIds.push({
+                  telegramId: String(messageSent.chat.id),
+                  messageId: messageSent.message_id
+                });
+                successed += 1;
+                resolve();
+              })
+              .catch(async error => {
+                errored += 1;
+                if (!error.response) {
+                  logger.error(
+                    { error: error.message, telegram_id: user.telegram_id, message },
+                    'Error while sending Global Notification telegram message'
+                  );
+                } else if (
+                  error.response.description === 'Forbidden: bot was blocked by the user' ||
+                  error.response.description === 'Forbidden: user is deactivated'
+                ) {
+                  logger.info(
+                    {
+                      error: error.response,
+                      telegram_id: user.telegram_id,
+                      message
+                    },
+                    'Telegram user marked as blocked'
+                  );
+                  await setUserBlocked.execute(user.telegram_id);
+                } else {
+                  logger.error(
+                    {
+                      error: error.response,
+                      telegram_id: user.telegram_id,
+                      message
+                    },
+                    'Error while sending Global Notification telegram message'
+                  );
+                }
+                reject();
+              });
+          }, 150 * index);
+        })
+    );
 
-              errored += 1;
-            });
+    await Promise.allSettled(promises);
 
-          if (index === array.length - 1) {
-            await bot.instance.api.sendMessage(
-              608520255,
-              `The messages were sent!\n\nSuccessed: ${successed}\nErrored: ${errored}`,
-              { parse_mode: 'HTML' }
-            );
-          }
-        }, 150 * index);
-      })
+    const id = Math.floor(new Date().getTime() * Math.random()).toString();
+    await redisProvider.save(`globalNotificationMessages:${id}`, messageIds, 'EX', 300);
+
+    await bot.instance.api.sendMessage(
+      608520255,
+      `The messages were sent!\n\nID: ${id}\n\n<b>${successed}/${unblockedUsers.length} successed (${errored} failed)</b>`,
+      { parse_mode: 'HTML' }
     );
   }
 }
