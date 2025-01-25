e184b03f7d6bc2da3f74bb37d84186a104bb103a
ninjastic <ninjasticdev@protonmail.com>
2025-01-24 22:36:28 -0300

    fix: notificationrepository type

--- src/modules/notifications/infra/typeorm/dtos/CreateNotificationDTO.ts
+++ src/modules/notifications/infra/typeorm/dtos/CreateNotificationDTO.ts
@@ -1,5 +1,7 @@
+import { NotificationType } from '@/modules/notifications/infra/typeorm/entities/Notification';
+
 export interface CreateNotificationDTO {
   telegram_id: string;
-  type: string;
+  type: NotificationType;
   metadata: object;
 }
