8d16908a7b52a1f5e0eac0047237aa5c4bf6c61d
ninjastic <ninjasticdev@protonmail.com>
2025-03-10 17:44:55 -0300

    feat: mixtum sponsor to rotation

--- src/config/sponsor.ts
+++ src/config/sponsor.ts
@@ -1,4 +1,5 @@
 export const sponsorTexts = [
   '\n\n» Sponsored by <a href="https://bc.game/">BC.Game</a>',
-  '\n\n» Sponsored by <a href="https://t.me/whale/?start=btctalktg">@Whale</a>'
+  '\n\n» Sponsored by <a href="https://t.me/whale/?start=btctalktg">@Whale</a>',
+  '\n\n» Sponsored by <a href="https://mixtum.io/">MixTum</a>'
 ];
