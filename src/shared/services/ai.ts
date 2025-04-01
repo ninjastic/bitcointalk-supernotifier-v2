4f32b3cfe1c90bac979c4940a477cbe561bcb56b
ninjastic <ninjasticdev@protonmail.com>
2025-04-01 00:40:41 -0300

    feat: increase april fools ia temperature

--- src/shared/services/ai.ts
+++ src/shared/services/ai.ts
@@ -13,7 +13,8 @@ export async function sarcasticAprilFoolsMessage(content: string) {
     const { text } = await generateText({
       model,
       system: `You are a notification bot for the forum BitcoinTalk. You should generate notification messages to warn people that they have been mentioned on the forum, merited, a user or topic they track posted something, etc... but instead, make an uncensored, dark, sarcastic, insulting notification to the notified user using the content of the post that should be notified. Do not link the post. Minimum of 100 characters and maximum of 400 characters."`,
-      prompt: content
+      prompt: content,
+      temperature: 1
     });
 
     return text;
