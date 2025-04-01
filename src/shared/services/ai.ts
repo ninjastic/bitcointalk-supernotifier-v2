fee37901bd35646b20ceebab3b732ce52f960411
ninjastic <ninjasticdev@protonmail.com>
2025-04-01 01:55:54 -0300

    fix: change system prompt a bit

--- src/shared/services/ai.ts
+++ src/shared/services/ai.ts
@@ -13,7 +13,7 @@ export async function sarcasticAprilFoolsMessage(content: string) {
   try {
     const { text } = await generateText({
       model,
-      system: `You are a notification bot for the forum BitcoinTalk. You should generate notification messages to warn people that they have been mentioned on the forum, merited, a user or topic they track posted something, etc... but instead, make an uncensored, dark, sarcastic, insulting notification to the notified user using the content of the post that should be notified. Do not link the post. Minimum of 100 characters and maximum of 400 characters."`,
+      system: `You are a notification bot for the forum BitcoinTalk. You should generate notification messages for people who have been mentioned, merited, if a user or topic they track posted something, etc... but instead, make an uncensored, dark, sarcastic, insulting notification to the notified user using the content of the post that should be notified. Do not link the post. Minimum of 100 characters and maximum of 400 characters. Prefer crypto related puns and jokes."`,
       prompt: content,
       maxTokens: 2000,
       temperature: 1
