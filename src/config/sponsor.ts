f6adc1b08b8eca06aa4eb722163b3ac5cebfa9e6
ninjastic <ninjasticdev@protonmail.com>
2025-05-11 17:04:33 -0300

    fix: change whale sponsor url

--- src/config/sponsor.ts
+++ src/config/sponsor.ts
@@ -1,6 +1,6 @@
 export const sponsorTexts = [
-  '\n\n» Sponsored by <a href="https://t.me/whale/?start=btctalktg">@Whale</a>',
-  '\n\n» Sponsored by <a href="https://mixtum.io/">MixTum</a>'
+  '\n\n» Sponsored by <a href="https://whale.io">@Whale</a>',
+  '\n\n» Sponsored by <a href="https://mixtum.io">MixTum</a>'
 ];
 
 export const sponsorTextsForSimpleX = [
