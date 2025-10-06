0f6add3bbb07493016fd9abc278d00455fa70fa8
ninjastic <ninjasticdev@protonmail.com>
2025-10-06 18:25:02 -0300

    feat: remove whale ad

--- src/config/sponsor.ts
+++ src/config/sponsor.ts
@@ -1,9 +1,7 @@
 export const sponsorTexts = [
-  '\n\n» Sponsored by <a href="https://whale.io">@Whale</a>',
   '\n\n» Sponsored by <a href="https://mixtum.io">MixTum</a>'
 ];
 
 export const sponsorTextsForSimpleX = [
-  '\n\n» Sponsored by https://whale.io',
   '\n\n» Sponsored by https://mixtum.io'
 ];
