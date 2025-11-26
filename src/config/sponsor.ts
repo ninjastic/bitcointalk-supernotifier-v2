95e7c9745036628f744d60da133373a0bc460f8a
ninjastic <ninjasticdev@protonmail.com>
2025-11-26 01:17:53 -0300

    feat: mixtum sponsor message

--- src/config/sponsor.ts
+++ src/config/sponsor.ts
@@ -1,3 +1,3 @@
-export const sponsorTexts = ['\n\n» Sponsored by <a href="https://mixtum.io">MixTum</a>'];
+export const sponsorTexts = ['\n\n» Sponsored by <a href="https://mixtum.io">MixTum Mixer 🛡</a>'];
 
 export const sponsorTextsForSimpleX = ['\n\n» Sponsored by https://mixtum.io'];
