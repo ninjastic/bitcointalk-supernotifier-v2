96153ad1b5c8ad22b2aa755933cf85364b518c0e
ninjastic <ninjasticdev@protonmail.com>
2026-03-31 22:17:27 -0300

    feat: low model reasoning

--- src/shared/services/ai.ts
+++ src/shared/services/ai.ts
@@ -1,13 +1,12 @@
-import { createOpenAI } from '@ai-sdk/openai';
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
 import logger from '##/shared/services/logger';
 import { generateText } from 'ai';
 
-const openAi = createOpenAI({
-  baseURL: 'https://openrouter.ai/api/v1',
+const openRouter = createOpenRouter({
   apiKey: process.env.OPENROUTER_APIKEY,
 });
 
-const model = openAi('x-ai/grok-4.1-fast');
+const model = openRouter('x-ai/grok-4.1-fast');
 
 export async function sarcasticAprilFoolsMessage(content: string) {
   try {
@@ -15,8 +14,15 @@ export async function sarcasticAprilFoolsMessage(content: string) {
       model,
       system: `You are a notification bot for the forum BitcoinTalk. You should generate notification messages for people who have been mentioned, merited, if a user or topic they track posted something, etc... instead, roast the user in a sarcastic, insulting, joking notification to the notified user using the content of the post. Do not link the post. Min of 50 characters and max of 200 characters. Prefer crypto and bitcointalk related puns and jokes.`,
       prompt: content,
-      maxTokens: 1000,
+      maxOutputTokens: 1000,
       temperature: 1,
+      providerOptions: {
+        openrouter: {
+          reasoning: {
+            effort: 'none',
+          },
+        },
+      },
     });
 
     return text;
