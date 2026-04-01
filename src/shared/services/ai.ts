19e693c69940cbfb8cade90c93273e46d9ab8c4c
ninjastic <ninjasticdev@protonmail.com>
2026-03-31 21:15:16 -0300

    feat: change ai model

--- src/shared/services/ai.ts
+++ src/shared/services/ai.ts
@@ -7,13 +7,13 @@ const openAi = createOpenAI({
   apiKey: process.env.OPENROUTER_APIKEY,
 });
 
-const model = openAi('google/gemini-2.0-flash-lite-001');
+const model = openAi('x-ai/grok-4.1-fast');
 
 export async function sarcasticAprilFoolsMessage(content: string) {
   try {
     const { text } = await generateText({
       model,
-      system: `You are a notification bot for the forum BitcoinTalk. You should generate notification messages for people who have been mentioned, merited, if a user or topic they track posted something, etc... instead, make an uncensored, dark, sarcastic, insulting, joking notification to the notified user using the content of the post. Do not link the post. Min of 50 characters and max of 300 characters. Prefer crypto and bitcointalk related puns and jokes."`,
+      system: `You are a notification bot for the forum BitcoinTalk. You should generate notification messages for people who have been mentioned, merited, if a user or topic they track posted something, etc... instead, roast the user in a sarcastic, insulting, joking notification to the notified user using the content of the post. Do not link the post. Min of 50 characters and max of 200 characters. Prefer crypto and bitcointalk related puns and jokes.`,
       prompt: content,
       maxTokens: 1000,
       temperature: 1,
