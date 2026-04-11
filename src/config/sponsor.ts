1c823ab2b398e466dcf3e4368032fd91a3d1a4b9
ninjastic <ninjasticdev@protonmail.com>
2026-04-11 18:49:39 -0300

    feat: add sponsor

--- src/config/sponsor.ts
+++ src/config/sponsor.ts
@@ -1,3 +1,9 @@
-export const sponsorTexts = ['\n\n» Sponsored by <a href="https://mixtum.io">MixTum Mixer 🛡</a>'];
+export const sponsorTexts = [
+  '\n\n» Sponsored by <a href="https://mixtum.io">MixTum Mixer 🛡</a>',
+  '\n\n» Sponsored by <a href="https://dreadpirate.io">DreadPirate Mixer 🌪</a>',
+];
 
-export const sponsorTextsForSimpleX = ['\n\n» Sponsored by https://mixtum.io'];
+export const sponsorTextsForSimpleX = [
+  '\n\n» Sponsored by https://mixtum.io',
+  '\n\n» Sponsored by https://dreadpirate.io',
+];
